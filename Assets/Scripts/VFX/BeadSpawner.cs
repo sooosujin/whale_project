8688a9a73ee3a1ec077f3b1ec2a734f17fffaa06
sooosujin <sjvenus94@gmail.com>
2025-10-30 17:07:52 +0900

    누락된 스크립트 추가

--- Assets/Scripts/VFX/BeadSpawner.cs
+++ Assets/Scripts/VFX/BeadSpawner.cs
@@ -2,46 +2,27 @@ using UnityEngine;
 
 public class BeadSpawner : MonoBehaviour
 {
-    public GameObject beadPrefab;
-    public int spawnOnStart = 0; // 테스트용
-    public float radius = 8f;
-    public float sinkSpeed = 0.5f;
+  [SerializeField] private GameObject tearBallPrefab;
+    [SerializeField] private Transform spawnAreaTop; // 상단 범위(박스)
+    [SerializeField] private Vector2 spawnBoxSize = new Vector2(8f, 0f);
+    [SerializeField] private float spawnHeight = 6f;
 
-    public static int TotalBeads
+    public GameObject SpawnBall(AudioClip clip)
     {
-        get;
-        private set;
+        if (!tearBallPrefab) return null;
+
+        Vector3 pos = GetRandomTopPosition();
+        var go = Instantiate(tearBallPrefab, pos, Quaternion.identity);
+        var ball = go.GetComponent<TearBall>();
+        if (ball) ball.BindAudio(clip);
+
+        return go;
     }
 
-    public void SpawnBead()
+    private Vector3 GetRandomTopPosition()
     {
-        Vector3 pos = transform.position + Random.insideUnitSphere * radius;
-        pos.y = Random.Range(2f, 6f);
-        var go = Instantiate(beadPrefab, pos, Quaternion.identity);
-        go.AddComponent<BeadSink>().speed = sinkSpeed;
-        TotalBeads++;
-    }
-
-    // 외부에서 CSV 레코드 수만큼 호출하면 됨
-    public void SpawnFromCSVCount(int count)
-    {
-        for (int i = 0; i < count; i++) SpawnBead();
-    }
-
-}
-
-public class BeadSink : MonoBehaviour
-{
-    public float speed = 0.5f;
-    float floorY = -2f;
-    void Update()
-    {
-        transform.position += Vector3.down * speed * Time.deltaTime;
-        if (transform.position.y < floorY) // 바닥 도달하면 약간 흔들림만 유지하거나 멈춤
-        {
-            var pos = transform.position; pos.y = floorY;
-            transform.position = pos;
-            enabled = false;
-        }
+        Vector3 center = spawnAreaTop ? spawnAreaTop.position : Vector3.zero;
+        float x = Random.Range(-spawnBoxSize.x * 0.5f, spawnBoxSize.x * 0.5f);
+        return new Vector3(center.x + x, center.y + spawnHeight, center.z);
     }
 }
